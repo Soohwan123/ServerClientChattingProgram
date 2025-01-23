1e8ba4ba0780bf6aa1d6a3903d1e4466a2c3a7ce
Soohwan123 <90574797+Soohwan123@users.noreply.github.com>
2025-01-23 07:51:56 -0500

    Update utils.c

--- server/utils.c
+++ server/utils.c
@@ -6,13 +6,16 @@
 
 // 클라이언트들에게 메시지를 브로드캐스트
 void broadcast_message(char *message, int sender_socket) {
-	pthread_mutex_lock(&clients_mutex); // 잠금
-	
-	for(int i = 0; i < MAX_CLIENTS; i++) {
-		if (clients[i] && clients[i]->socket != sender_socket){
-			send(clients[i]->socket, message, strlen(message), 0);
-		}
-	}
+    pthread_mutex_lock(&clients_mutex); // 뮤텍스 잠금
 
-	pthread_mutex_unlock(&clients_mutex); //잠금 해체
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i] && clients[i]->socket != sender_socket) {
+            if (send(clients[i]->socket, message, strlen(message), 0) < 0) {
+                perror("Send failed");
+                remove_client(clients[i]->socket);  // **수정**: 직접 메모리 해제 대신 remove_client 호출
+            }
+        }
+    }
+
+    pthread_mutex_unlock(&clients_mutex); // 뮤텍스 잠금 해제
 }
